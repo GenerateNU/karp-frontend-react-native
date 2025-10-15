a22ec74caa5eb91e9a33632908c27ca343b40b97
Victoria Chin <victoriachin@Victorias-MacBook-Air.local>
2025-10-15 03:28:58 -0400

    Fixed the styling and such

--- frontend/app/(tabs)/store.tsx
+++ frontend/app/(tabs)/store.tsx
@@ -3,6 +3,7 @@ import { FlatList, TouchableOpacity, TextInput } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { Fonts } from '@/constants/Fonts';
 
 const STORE_ITEMS = [
   { id: '1', name: 'Cookie', store: 'Crumbl', coins: 150 },
@@ -27,63 +28,144 @@ export default function StoreScreen() {
   const renderItem = ({ item }: { item: (typeof STORE_ITEMS)[0] }) => (
     <TouchableOpacity
       key={item.id}
-      className="mx-1 w-[40%]" // closer together
+      className="mx-2"
       onPress={() => handlePress(item.id)}
       activeOpacity={0.8}
     >
       <ThemedView
-        lightColor="#FFFFFF"
-        darkColor="#FFFFFF"
-        className="items-center rounded-xl p-2 shadow-md"
+        lightColor="#E5E5E5"
+        darkColor="#E5E5E5"
+        className="p2 rounded-xl shadow-md"
+        style={{ width: 180, height: 160 }}
       >
+        {/* Photo place holder*/}
         <ThemedView
-          className="mb-2 h-20 w-full rounded-md bg-gray-300"
-          lightColor="#D0D0D0"
-          darkColor="#D0D0D0"
+          className="mb-2 w-full rounded-md bg-gray-400"
+          style={{ width: 180, height: 100 }}
+          lightColor="#D3D3D3"
+          darkColor="#D3D3D3"
         />
         <ThemedText
-          type="subtitle"
-          className="text-center text-base font-semibold text-black"
+          type="title"
+          style={{
+            textAlign: 'left',
+            color: 'black',
+            fontSize: '20',
+            paddingLeft: '5',
+            fontFamily: Fonts.regular_400,
+          }}
         >
           {item.name}
         </ThemedText>
-        <ThemedText
-          type="subtitle"
-          className="text-center text-base font-semibold text-black"
+        <ThemedView
+          lightColor="#E5E5E5"
+          darkColor="#E5E5E5"
+          className="flex-row items-center justify-between"
+          style={{ width: '100%' }}
         >
-          {item.store}
-        </ThemedText>
-        <ThemedText
-          type="body"
-          className="mt-1 text-center text-base font-bold text-black"
-        >
-          {item.coins} coins
-        </ThemedText>
+          <ThemedText
+            type="subtitle"
+            style={{
+              color: 'black',
+              textAlign: 'left',
+              fontSize: '12',
+              paddingLeft: '5',
+              fontFamily: Fonts.regular_400,
+            }}
+          >
+            {item.store}
+          </ThemedText>
+          <ThemedText
+            type="body"
+            style={{
+              color: 'black',
+              textAlign: 'right',
+              paddingRight: '5',
+              fontFamily: Fonts.regular_400,
+            }}
+          >
+            {item.coins} coins
+          </ThemedText>
+        </ThemedView>
       </ThemedView>
     </TouchableOpacity>
   );
 
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#1E90FF', dark: '#1E90FF' }}
+      headerBackgroundColor={{ light: '#8ecde8', dark: '#8ecde8' }}
       headerImage={
         <ThemedView
-          lightColor="#1E90FF"
-          darkColor="#1E90FF"
-          className="flex-1 items-center justify-end pb-10"
+          lightColor="#8ecde8"
+          darkColor="#8ecde8"
+          className="flex-1 items-center justify-end pb-5"
         >
           <ThemedText
             type="title"
             className="text-center text-3xl font-extrabold text-white"
+            style={{ fontFamily: Fonts.regular_400 }}
           >
             Gift Shop
           </ThemedText>
+
+          <ThemedView
+            lightColor="#8ecde8"
+            darkColor="#8ecde8"
+            className="mt-6 w-full flex-row items-center justify-between px-6"
+          >
+            <ThemedText
+              type="subtitle"
+              style={{
+                color: 'white',
+                fontSize: 20,
+                fontFamily: Fonts.medium_500,
+              }}
+            >
+              Level 10
+            </ThemedText>
+
+            <ThemedText
+              type="subtitle"
+              style={{
+                color: 'white',
+                fontSize: 20,
+                fontFamily: Fonts.medium_500,
+              }}
+            >
+              500 coins
+            </ThemedText>
+
+            <TouchableOpacity
+              onPress={() => {
+                throw new Error('History button not implemented yet');
+              }}
+              style={{
+                backgroundColor: 'rgba(12, 120, 128, 0.5)', // semi-dark circle
+                paddingHorizontal: 10, // allow text to fit
+                paddingVertical: 10,
+                borderRadius: 20,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <ThemedText
+                type="subtitle"
+                style={{
+                  color: 'white',
+                  fontSize: 20,
+                  fontFamily: Fonts.medium_500,
+                }}
+              >
+                History &gt;
+              </ThemedText>
+            </TouchableOpacity>
+          </ThemedView>
         </ThemedView>
       }
     >
       <ThemedView
-        lightColor="#FFFFFF"
-        darkColor="#FFFFFF"
+        lightColor="#F2F2F2"
+        darkColor="#FFFFF"
         className="flex-1 px-4"
       >
         <TextInput
@@ -94,17 +176,70 @@ export default function StoreScreen() {
           onChangeText={setSearchText}
         />
 
-        {[0, 1, 2].map(index => (
-          <FlatList
-            key={index}
-            horizontal
-            data={filteredItems}
-            keyExtractor={item => item.id}
-            renderItem={renderItem}
-            showsHorizontalScrollIndicator={false}
-            contentContainerStyle={{ paddingHorizontal: 4, marginBottom: 20 }}
-          />
-        ))}
+        {/* Popular in Boston */}
+        <ThemedText
+          className="mb-2 text-lg font-bold text-black"
+          style={{
+            color: 'black',
+            textAlign: 'left',
+            marginTop: 4,
+            fontFamily: Fonts.regular_400,
+            fontSize: '20',
+          }}
+        >
+          Popular in Boston
+        </ThemedText>
+        <FlatList
+          horizontal
+          data={filteredItems.slice(0, 3)}
+          keyExtractor={item => item.id}
+          renderItem={renderItem}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: 8, marginBottom: 20 }}
+        />
+
+        <ThemedText
+          className="mb-2 text-lg font-bold text-black"
+          style={{
+            color: 'black',
+            textAlign: 'left',
+            marginTop: 4,
+            fontFamily: Fonts.regular_400,
+            fontSize: '20',
+          }}
+        >
+          Sweet Treats
+        </ThemedText>
+        <FlatList
+          horizontal
+          data={filteredItems.slice(2, 5)}
+          keyExtractor={item => item.id}
+          renderItem={renderItem}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: 8, marginBottom: 20 }}
+        />
+
+        {/* Rewards */}
+        <ThemedText
+          className="mb-2 text-lg font-bold text-black"
+          style={{
+            color: 'black',
+            textAlign: 'left',
+            marginTop: 4,
+            fontFamily: Fonts.regular_400,
+            fontSize: '20',
+          }}
+        >
+          Shopping Spree
+        </ThemedText>
+        <FlatList
+          horizontal
+          data={filteredItems.slice(3, 6)}
+          keyExtractor={item => item.id}
+          renderItem={renderItem}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: 8, marginBottom: 20 }}
+        />
       </ThemedView>
     </ParallaxScrollView>
   );
