81c924d48a685e7c15c5dafc213de2141419fc14
Adam Ma <adam.ma@klaviyo.com>
2025-10-15 22:57:12 -0400

    Fix

---
+++ frontend/types/api/user.ts
@@ -0,0 +1,13 @@
+export type LoginResponse = {
+  accessToken: string;
+  tokenType: string;
+  user: {
+    id: string;
+    email?: string;
+    username: string;
+    firstName?: string;
+    lastName?: string;
+    userType?: string;
+    entityId?: string | null;
+  };
+};
